32684fdf540b1b4cf87b2f3bb9883e85a84522d1
Josef Svenningsson <josef.svenningsson@gmail.com>
2009-11-26 22:28:41 +0100

    Added a small benchmarking program to test the speed of creating a tree
    darcs-hash:20091126212841-f316c-e2993de5f30dff54f4ce56e12a4ab8db71bef37e

---
+++ bench/Bench.hs
@@ -0,0 +1,14 @@
+module Main where
+{-
+A simple module to test how long it takes to build up a tree.
+The test data file is taken from Facebook 
+http://www.facebook.com/careers/puzzles.php?puzzle_id=17
+
+Thanks to Wei Hu (weihu@cs.virginia.edu) for alerting me about the
+slowness of my previous implementation.
+-}
+
+import Data.Set.BKTree
+
+main = do file <- readFile "twl06.txt"
+          print (size (fromList (lines file)))
